ac63577c057da8a76db43c721b4ee0d572d17bd6
juns <wer3308@gmail.com>
2025-03-29 22:47:16 +0900

    upload Atcoder ABC 399 code

---
+++ atcoder/250329 ABC 399/4.py
@@ -0,0 +1,18 @@
+#基本操作
+import sys
+input = sys.stdin.readline
+from collections import deque
+import bisect
+import heapq
+import math
+import copy
+
+#input 
+n = int(input())
+lst = list(map(int, input().split()))
+for i in range():
+while True:
+while q:
+
+#solve 
+
