2b96314211dd782c0be282f13de15cf5758181b2
juns <wer3308@gmail.com>
2025-03-29 23:45:56 +0900

    ファイル間違ったのでre_update

--- atcoder/250329 ABC 399/4.py
+++ atcoder/250329 ABC 399/4.py
@@ -1,18 +1,43 @@
 #基本操作
 import sys
 input = sys.stdin.readline
-from collections import deque
-import bisect
-import heapq
-import math
-import copy
+from collections import defaultdict
 
-#input 
-n = int(input())
-lst = list(map(int, input().split()))
-for i in range():
-while True:
-while q:
+dx = [1]
+tc = int(input())
 
-#solve 
+for _ in range(tc):
+    ans = 0
 
+
+    n = int(input())
+    dic = defaultdict(list)
+    lst = list(map(int, input().split()))
+    ban_list = []
+    for i  in range(2 * n):
+        now = lst[i]
+        piv = [i - 1, i + 1]
+        for j in piv:
+            if 0 <=  j < 2 * n:
+                pivv = lst[j]
+                if now == pivv:
+                    ban_list.append(now)
+
+
+
+    for i in range(2 * n):
+        now = lst[i]
+        piv = [i - 1, i + 1]
+        for j in piv:
+            if 0 <=  j < 2 * n:
+                pivv = lst[j]
+
+                if pivv in dic[now]:
+                    if pivv in ban_list or now in ban_list:
+                        continue
+                    ans += 1
+                    continue
+                dic[now].append(pivv)
+
+
+    print(ans // 2)
